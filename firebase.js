06f0a22f63dc213e4b7033ae0f1d0930bcfda32a
Gouravbirwaz <148947155+Gouravbirwaz@users.noreply.github.com>
2024-11-11 20:30:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,14 +5,7 @@ import { getDatabase, ref, set } from 'firebase/database';  // Import Realtime D
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFHxt83jUiWNzhUSX63qcq6xDVCsWUBlw",
-  authDomain: "gemhack-a9246.firebaseapp.com",
-  databaseURL: "https://gemhack-a9246-default-rtdb.asia-southeast1.firebasedatabase.app", // Realtime Database URL
-  projectId: "gemhack-a9246",
-  storageBucket: "gemhack-a9246.firebasestorage.app",
-  messagingSenderId: "118483115946",
-  appId: "1:118483115946:web:65c9b5e60c7a123c86d5d3",
-  measurementId: "G-Y075DR7QNE"
+ //your firebase configuration;
 };
 
 const app = initializeApp(firebaseConfig);
